6f70de6237cf0e89073596e4902dc7aa2a527c1b
Yulia Uralova <julia.uralova@gmail.com>
2025-12-05 22:38:10 +0300

    Задание #1. Обработка исключений

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.util.Scanner;
+
+
+class VeryLongLineException extends RuntimeException {
+    public VeryLongLineException(String message) {
+        super(message);
+    }
+}
 
 public class Main {
     public static void main(String[] args) {
@@ -28,6 +37,41 @@ public class Main {
             System.out.println("Путь указан верно");
             correctFileCount++;
             System.out.println("Это файл номер " + correctFileCount);
+
+            int lineCounter = 0;
+            int longestLine = 0;
+            int shortestLine = Integer.MAX_VALUE;
+
+
+            try {
+                FileReader fileReader = new FileReader(path);
+                BufferedReader reader = new BufferedReader(fileReader);
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    int length = line.length();
+
+                    if (length > 1024) {
+                        throw new VeryLongLineException("Строка превышает 1024 символа");
+                    }
+
+                    lineCounter ++;
+
+                    if (length > longestLine) {
+                        longestLine = length;
+                    }
+
+                    if (length < shortestLine) {
+                        shortestLine = length;
+                    }
+                }
+                reader.close();
+            } catch (Exception ex) {
+                    ex.printStackTrace();
+                }
+
+            System.out.println("Общее количество строк в файле: " + lineCounter);
+            System.out.println("Длина самой длинной строки в файле: " + longestLine);
+            System.out.println("Длина самой короткой строки в файле: " + shortestLine);
+            }
         }
     }
-}
